95d493e90258260ef987b2bb8c88083b49380bf7
Abdelali12-codes <jadelmoulaa3@gmail.com>
2022-07-08 10:58:14 +0100

    first push

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [getMessage, setGetMessage] = useState({})
+
+  useEffect(()=>{
+    axios.get('https://flaskeb.abdelalitraining.com/hello').then(response => {
+      console.log("SUCCESS", response)
+      setGetMessage(response)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>React + Flask Tutorial</p>
+        <div>{getMessage.status === 200 ? 
+          <h3>{getMessage.data.message}</h3>
+          :
+          <h3>LOADING</h3>}</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
